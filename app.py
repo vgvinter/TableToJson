59abb3b58af3766f33371a1396e5ca956e7803ff
M Victoria Gil <94982919+vgvinter@users.noreply.github.com>
2023-05-29 20:29:24 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,148 @@
+import json
+
+from IPython.display import HTML
+import streamlit as st
+
+from utils import highlight_values
+
+
+
+def main():
+
+    st.set_page_config(
+        page_title="LLMs: JSON structured tables from scientific papers", 
+        layout="wide"
+    )
+    st.title(":page_facing_up: LLMs: JSON structured tables from scientific papers")
+
+    st.markdown("""
+        <style>
+                .block-container {
+                    padding-top: 2rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+    
+
+    col1, col2, col3 = st.columns([1,8,1])
+
+    with col2: 
+        st.markdown('<p style="font-family: sans-serif; font-weight: bold; color:steelblue; \
+                font-size: 24px;">DOI of the article containing the table:</p>', unsafe_allow_html=True)
+         
+        table = st.selectbox(
+            "Choose a DOI",
+            ("10.1016/j.jece.2023.109643", "10.1021/ja4045289", "10.1016/j.est.2023.107335", 
+            "10.1021/acs.energyfuels.1c02406", "10.1016/j.enconman.2018.09.020", "10.1039/D3NJ00316G"), 
+            label_visibility="collapsed"
+        )
+
+
+        if table == "10.1016/j.jece.2023.109643":
+            col1, col2 = st.columns([1,0.6])
+            with col1:
+                with open("html_tables/html_table_carbonsCO2ads.txt", 'r') as file:
+                    html_table_carbonsCO2ads = file.read()
+                    file.close()
+                st.write(HTML(html_table_carbonsCO2ads))
+            with col2:
+                with open("structured_json_results/json_table_carbonsCO2ads.json") as file:
+                    json_table_carbonsCO2ads = json.load(file)
+                    file.close()
+                st.json(json_table_carbonsCO2ads)
+
+
+        if table == "10.1021/ja4045289":
+            col1, col2 = st.columns([1,0.6])
+            with col1:
+                with open("html_tables/html_table_MOFproperties.txt", 'r') as file:
+                    html_table_MOFproperties = file.read()
+                    file.close()
+                st.write(HTML(html_table_MOFproperties))
+            with col2:
+                with open("structured_json_results/json_table_MOFproperties.json") as file:
+                    json_table_MOFproperties = json.load(file)
+                    file.close()
+                st.json(json_table_MOFproperties)
+
+
+        if table == "10.1016/j.est.2023.107335":
+            col1, col2 = st.columns([1,0.6])
+            with col1:
+                with open("html_tables/html_table_supercapacitor.txt", 'r') as file:
+                    html_table_supercapacitor = file.read()
+                    file.close()
+                st.write(HTML(html_table_supercapacitor))
+            with col2:
+                with open("structured_json_results/json_table_supercapacitor.json") as file:
+                    json_table_supercapacitor = json.load(file)
+                    file.close()
+                st.json(json_table_supercapacitor)
+
+
+        if table == "10.1021/acs.energyfuels.1c02406":
+            with open("html_tables/html_table_catalystCO2.txt", 'r') as file:
+                html_table_catalystCO2 = file.read()
+                file.close()
+            st.write(HTML(html_table_catalystCO2))
+            st.write("")
+            st.write("")
+            col1, col2, col3 = st.columns([1,2.7,1])
+            with col2:
+                with open("structured_json_results/json_table_catalystCO2.json") as file:
+                    json_table_catalystCO2 = json.load(file)
+                    file.close()
+                st.json(json_table_catalystCO2)
+
+
+        if table == "10.1016/j.enconman.2018.09.020":
+            with open("html_tables/html_table_biomass.txt", 'r') as file:
+                html_table_biomass = file.read()
+                file.close()
+            st.write(HTML(html_table_biomass))
+            st.write("")
+            st.write("")
+            col1, col2, col3 = st.columns([1,2.7,1])
+            with col2:
+                with open("structured_json_results/json_table_biomass.json") as file:
+                    json_table_biomass = json.load(file)
+                    file.close()
+                st.json(json_table_biomass)
+
+        
+        if table == "10.1039/D3NJ00316G":
+            with open("html_tables/html_table_anodesSOFCs.txt", 'r') as file:
+                html_table_anodesSOFCs = file.read()
+                file.close()
+            st.write(HTML(html_table_anodesSOFCs))
+            st.write("")
+            st.write("")
+
+            col1, col2 = st.columns(2)
+            with col1:
+                st.markdown('<p style="font-family: sans-serif; font-weight: bold; color:steelblue; \
+                            font-size: 20px;">JSON with the wrong promt</p>', unsafe_allow_html=True)
+                st.markdown('<p style="font-family: sans-serif; font-weight: normal; color:grey; \
+                            font-size: 17px;">prompt: "Generate a object with the following schema \
+                            extracting the information from the provided table in html code:"</p>', 
+                            unsafe_allow_html=True)
+                st.write("")
+                with open("structured_json_results/json_table_anodesSOFCs_wrong.json") as file:
+                    json_table_anodesSOFCs_wrong = json.load(file)
+                    file.close()
+                st.json(json_table_anodesSOFCs_wrong)
+            with col2:
+                st.markdown('<p style="font-family: sans-serif; font-weight: bold; color:steelblue; \
+                            font-size: 20px;">JSON with the improved promt</p>', unsafe_allow_html=True)
+                st.markdown('<p style="font-family: sans-serif; font-weight: normal; color:grey; \
+                            font-size: 17px;">prompt: ""Generate a object with the following schema extracting \
+                            the information from the provided table in html code (if you find numbers as \
+                            1.025 × 10&lt;sub&gt;-3&lt;/sub&gt;</code>, this means 1.025e-3):"</p>', unsafe_allow_html=True)
+                with open("structured_json_results/json_table_anodesSOFCs.json") as file:
+                    json_table_anodesSOFCs = json.load(file)
+                    file.close()
+                st.json(json_table_anodesSOFCs)
+
+
+if __name__ == "__main__":
+    main()
---
+++ jsonformer_non_tokens.py
@@ -0,0 +1,243 @@
+import os
+import json
+import re
+from typing import Any, Dict, List, Optional, Union
+
+import openai
+
+
+GENERATION_MARKER = "|GENERATION|"
+
+
+class JsonformerNoTokens:
+    value: Dict[str, Any] = {}
+
+    def __init__(
+        self,
+        model: object,
+        json_schema: Dict[str, Any],
+        prompt: str,
+        text: Optional[str] = None,
+        debug: bool = False,
+        max_array_length: int = 10,
+        max_number_tokens: int = 6,
+        temperature: float = 0.1,
+        max_string_token_length: int = 10,
+    ):
+        self.model = model
+        self.json_schema = json_schema
+        self.prompt = prompt
+        self.text = text
+
+        self.generation_marker = "|GENERATION|"
+        self.debug_on = debug
+        self.max_array_length = max_array_length
+        self.max_number_tokens = max_number_tokens
+        self.temperature = temperature
+        self.max_string_token_length = max_string_token_length
+
+    def debug(self, *args, **kwargs):
+        if self.debug_on:
+            print(*args, **kwargs)
+
+    def generate_number(self, temperature: Union[float, None] = None, iterations=0):
+        prompt = self.get_prompt()
+        self.debug("[generate_number] prompt", prompt)
+        
+        response = self.model.generate(
+            prompt,
+            max_new_tokens=self.max_number_tokens,
+            stop=['",'],
+            temperature=temperature or self.temperature,
+        )
+        self.debug("[generate_number] response", response)
+
+        try:
+            numbers = re.finditer(r"\d+(\.\d+)*", response)
+            number = next(numbers).group(0)
+            if "." in number:
+                return float(number)
+            else:
+                return int(number)
+        except (ValueError, StopIteration):
+            self.debug(f"[generate_number] FAILED")
+            if iterations > 3:
+                raise ValueError("Failed to generate a valid number")
+            return self.generate_number(
+                temperature=self.temperature * 1.3, iterations=iterations + 1
+            )
+
+    def generate_boolean(self) -> bool:
+        prompt = self.get_prompt()
+        self.debug("[generate_boolean] prompt", prompt)
+
+        next_most_likely_token = self.model.get_next_most_likely_token(prompt)
+
+        if next_most_likely_token.lower() in ["true", "1"]:
+            return True
+        elif next_most_likely_token.lower() in ["false", "0"]:
+            return False
+        else:
+            print("Failed to generate a valid boolean value")
+            return None
+
+    def generate_string(self) -> str:
+        prompt = self.get_prompt()
+        self.debug("[generate_string] prompt", prompt)
+        
+        response = self.model.generate(
+            prompt,
+            max_new_tokens=self.max_string_token_length,
+            stop=['",'],
+            temperature=self.temperature,
+        )
+
+        self.debug("[generate_string] response", response)
+        split = response.split('"')
+        assert len(split) >= 2
+        return split[1].strip()
+
+    def generate_object(
+        self, 
+        properties: Dict[str, Any], 
+        obj: Dict[str, Any],
+    ) -> Dict[str, Any]:
+        # self.debug("[generate_object] properties", properties)
+        for key, schema in properties.items():
+            obj[key] = self.generate_value(schema, obj, key)
+        return obj
+
+    def generate_value(
+        self,
+        schema: Dict[str, Any],
+        obj: Union[Dict[str, Any], List[Any]],
+        key: Union[str, None] = None,
+    ) -> Any:
+        schema_type = schema["type"]
+        if schema_type == "number":
+            if key:
+                obj[key] = self.generation_marker
+            else:
+                obj.append(self.generation_marker)
+            return self.generate_number()
+        elif schema_type == "boolean":
+            if key:
+                obj[key] = self.generation_marker
+            else:
+                obj.append(self.generation_marker)
+            return self.generate_boolean()
+        elif schema_type == "string":
+            if key:
+                obj[key] = self.generation_marker
+            else:
+                obj.append(self.generation_marker)
+            return self.generate_string()
+        elif schema_type == "array":
+            new_array = []
+            obj[key] = new_array
+            return self.generate_array(schema["items"], new_array)
+        elif schema_type == "object":
+            new_obj = {}
+            if key:
+                obj[key] = new_obj
+            else:
+                obj.append(new_obj)
+            return self.generate_object(schema["properties"], new_obj)
+        else:
+            raise ValueError(f"Unsupported schema type: {schema_type}")
+
+    def generate_array(
+        self, 
+        item_schema: Dict[str, Any], 
+        obj: Dict[str, Any]
+    ) -> list:
+        for _ in range(self.max_array_length):
+            # forces array to have at least one element
+            element = self.generate_value(item_schema, obj)
+            obj[-1] = element
+
+            obj.append(self.generation_marker)
+            input_prompt = self.get_prompt()
+            obj.pop()
+            next_most_likely_token = self.model.get_next_most_likely_token(input_prompt)
+            if next_most_likely_token == "]":
+                break
+        return obj
+
+    def get_prompt(self):
+        template = """{prompt}\n{text}\nOutput result in the following JSON schema format, one value at a time, 
+        do not continue the JSON:\n{schema}\nResult: {progress}"""
+        progress = json.dumps(self.value)
+        gen_marker_index = progress.find(f'"{self.generation_marker}"')
+        if gen_marker_index != -1:
+            progress = progress[:gen_marker_index]
+        else:
+            raise ValueError("Failed to find generation marker")
+
+        prompt = template.format(
+            prompt=self.prompt,
+            text=self.text,
+            schema=json.dumps(self.json_schema),
+            progress=progress,
+        )
+        return prompt
+
+    def __call__(self) -> Dict[str, Any]:
+        self.value = {}
+        generated_data = self.generate_object(self.json_schema["properties"], self.value)
+        return generated_data
+
+
+class OpenAIModel:
+    def __init__(self, model_id: str = "text-curie-001", debug: bool = False):
+        self.model_id = model_id
+        self.debug_on = debug
+
+    def __request(
+        self,
+        prompt: str,
+        max_new_tokens: Optional[int] = None,
+        temperature: Optional[float] = None,
+        logprobs: Optional[int] = None,
+        **kwargs,
+    ):
+        response = openai.Completion.create(
+            model=self.model_id,
+            prompt=prompt,
+            temperature=temperature,
+            max_tokens=max_new_tokens,
+            logprobs=logprobs,
+        )
+        self.debug(response)
+        return response
+
+    def __clean_initial_new_lines(self, text: str):
+        # models often start responses with new lines
+        while text.startswith("\n"):
+            text = text[1:]
+        return text
+
+    def debug(self, *args, **kwargs):
+        if self.debug_on:
+            print(*args, **kwargs)
+
+    def generate(
+        self,
+        prompt: str,
+        max_new_tokens: Optional[int] = None,
+        temperature: Optional[float] = None,
+        **kwargs,
+    ):
+        response = self.__request(prompt, max_new_tokens=max_new_tokens, temperature=temperature)
+        return self.__clean_initial_new_lines(response.choices[0].text)
+
+    def get_next_most_likely_token(
+        self,
+        prompt: str,
+    ):
+        response = self.__request(prompt, logprobs=1)
+        for obj in response.choices[0].logprobs.top_logprobs:
+            # sorted from the most likely like so: [{"token": "logprob"}]
+            if "\n" in obj:
+                continue
+            return list(obj.keys())[0]
